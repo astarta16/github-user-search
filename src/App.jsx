01e2ef65fabf21c295c79a827de064423c6ef4b7
astarta16 <ngedeshuri2001@gmail.com>
2023-10-23 16:30:08 +0400

    add dark theme

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,55 @@
-import React from 'react';
-import styled from 'styled-components';
-import IconSearch from './assets/icon-search.svg';
+import React, { useState } from "react";
+import styled, { createGlobalStyle } from "styled-components";
+import Moon from "./assets/icon-moon.svg";
+import Sun from "./assets/icon-sun.svg";
 
-const SearchContainer = styled.div`
+const GlobalStyle = createGlobalStyle`
+  body {
+    background-color: ${(props) => (props.darkMode ? '#141D2F' : '#F6F8FF')};
+    color: ${(props) => (props.darkMode ? '#F6F8FF' : '#141D2F')};
+  }
+`;
+
+const Container = styled.div`
   display: flex;
-  flex-direction: column; /* To stack the input and the card vertically */
+  flex-direction: column;
   justify-content: center;
   align-items: center;
   width: 100vw;
   height: 100vh;
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  width: 730px;
+  flex-shrink: 0;
+  align-items: center;
+
+  @media (max-width: 768px) {
+    width: 90%;
+  }
+`;
+
+const Title = styled.h1`
+  margin: 0;
+  font-size: 26px;
+  font-weight: 700;
+`;
+
+const Theme = styled.p`
+  margin: 0;
+`;
+
+const DarkModeButton = styled.button`
+  background: none;
+  border: none;
+  cursor: pointer;
+`;
+
+const Icon = styled.img`
+  width: 24px;
+  height: 24px;
 `;
 
 const SearchInputContainer = styled.div`
@@ -24,25 +65,24 @@ const SearchInputContainer = styled.div`
   }
 `;
 
-const SearchInput = styled.div`
-  position: relative;
-  width: 100%;
-`;
-
 const Input = styled.input`
   padding: 15px 20px;
   border: 1px solid #ffffff;
   border-radius: 15px;
   font-size: 16px;
   width: 100%;
-  background: #ffffff;
   outline: none;
+  
+  &.dark-mode {
+    background-color: #1E2A47;
+    color: #ffffff;
+  }
 `;
 
 const SearchButton = styled.button`
   position: absolute;
   top: 50%;
-  right: 8px; 
+  right: 8px;
   transform: translateY(-50%);
   padding: 15px 20px;
   background-color: #007bff;
@@ -52,16 +92,19 @@ const SearchButton = styled.button`
   cursor: pointer;
   font-size: 16px;
   font-weight: 700;
-  background: #0079FF;
 `;
 
 const Card = styled.div`
   width: 730px;
   height: 419px;
-  background: #fff; 
-  margin-top: 20px; 
-  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1); 
+  margin-top: 20px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
   border-radius: 15px;
+
+  &.dark-mode {
+    background-color: #1E2A47;
+    color: #ffffff;
+  }
 
   @media (max-width: 768px) {
     width: 90%;
@@ -69,18 +112,31 @@ const Card = styled.div`
 `;
 
 const App = () => {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const toggleDarkMode = () => {
+    setDarkMode(!darkMode);
+  };
+
   return (
-    <SearchContainer>
+    <Container>
+      <GlobalStyle darkMode={darkMode} />
+      <Header>
+        <Title>DevFinder</Title>
+        <DarkModeButton onClick={toggleDarkMode}>
+          <Icon src={darkMode ? Sun : Moon} alt="Dark Mode" />
+        </DarkModeButton>
+      </Header>
       <SearchInputContainer>
-        <SearchInput>
-          <Input type="text" placeholder="Search GitHub username…" />
-        </SearchInput>
+        <Input
+          type="text"
+          placeholder="Search GitHub username…"
+          className={darkMode ? "dark-mode" : ""}
+        />
         <SearchButton>Search</SearchButton>
       </SearchInputContainer>
-      <Card>
-        {/* Card content goes here */}
-      </Card>
-    </SearchContainer>
+      <Card className={darkMode ? "dark-mode" : ""}></Card>
+    </Container>
   );
 };
 
